d34ecac9242ab5a6220b3de150ac14c00c0f446f
unknown <fadih1992@gmail.com>
2023-09-25 16:17:32 -0700

    first

--- main.py
+++ main.py
@@ -1,39 +1,3 @@
-#import nmap
-#
-#def scan_ip_range(start_ip, end_ip):
-#    nm = nmap.PortScanner()
-#
-#    for i in range(int(start_ip.split('.')[-1]), int(end_ip.split('.')[-1]) + 1):
-#        ip = start_ip.rsplit('.', 1)[0] + '.' + str(i)
-#        print(f"Scanning {ip}...")
-#
-#        # Perform a basic Nmap scan on the target IP
-#        nm.scan(ip, arguments='-sV --script vulners')
-#
-#        # Print the scan results
-#        print(f"Scan results for {ip}:")
-# #       print(nm[ip].csv())
-#        print('Host : %s (%s)' % (ip, nm[ip].hostname()))
-#
-#
-#        for proto in nm[ip].all_protocols():
-#           print('----------')
-#           print('Protocol : %s' % proto)
-#           
-#           lport = nm[ip][proto].keys()
-#
-#           print("lport: ",lport)
-#           #lport.sort()
-#           for port in lport:
-#              print ('port : %s\tstate : %s' % (port, nm[ip][proto][port]['state']))
-#
-#if __name__ == "__main__":
-#    start_ip = input("Enter the starting IP address: ")
-#    end_ip = input("Enter the ending IP address: ")
-#
-#    scan_ip_range(start_ip, end_ip)
-
-
 import nmap
 
 def scan_for_vulnerabilities(target_ip):
@@ -41,24 +5,19 @@ def scan_for_vulnerabilities(target_ip):
 
     print(f"Scanning {target_ip} for vulnerabilities...")
 
-    # Perform a vulnerability scan using Nmap
-    temp=nm.scan(target_ip, arguments='-sV --script vulners')
+     #Perform a vulnerability scan using Nmap
+    #temp=nm.scan(target_ip, arguments='-sV -O --script vulners')
 
+    temp=nm.scan(target_ip, arguments='-sV -O -PE')
     # Print the scan results
     print(f"Scanning target: {target_ip}")
-    print(f"Host is up: {temp['scan'][target_ip]['status']['state']}")
 
+    #print(temp)
+    print(f"Host status is: {temp['scan'][target_ip]['status']['state']}")
+    print(f"OS Name: {temp['scan'][target_ip]['osmatch'][0]['name']}")
     # Print the scanned ports and their service/version information
     for port, info in temp['scan'][target_ip]['tcp'].items():
         print(f"Port {port}: {info['name']} - {info['product']} {info['version']}")
-
-    # Print the vulnerabilities from the vulners NSE script
-    print("Vulnerabilities:")
-    if 'vulners' in temp['scan'][target_ip]:
-        vulnerabilities = temp['scan'][target_ip]['vulners']
-        for vuln in vulnerabilities:
-            print(vuln)
-    
 
 if __name__ == "__main__":
     target_ip = input("Enter the target IP address: ")
@@ -68,4 +27,4 @@ if __name__ == "__main__":
 
 
 # to find range of IPs
-#nmap -SN -PE 192.168.198.129-140
+#nmap -sN -PE 192.168.84.129-140
