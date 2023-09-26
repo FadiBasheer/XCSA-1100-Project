9b7c006d3dd1463633a03c30aeae8fcbb255fe7f
unknown <fadih1992@gmail.com>
2023-09-25 22:11:37 -0700

    first

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
-#import nmap
-#
-#def scan_for_vulnerabilities(target_ip):
-#    nm = nmap.PortScanner()
-#
-#    print(f"Scanning {target_ip} for vulnerabilities...")
-#
-#     #Perform a vulnerability scan using Nmap
-#    #temp=nm.scan(target_ip, arguments='-sV -O --script vulners')
-#
-#    temp=nm.scan(target_ip, arguments='-sV -O -PE')
-#    # Print the scan results
-#    print(f"Scanning target: {target_ip}")
-#
-#    #print(temp)
-#    print(f"Host status is: {temp['scan'][target_ip]['status']['state']}")
-#    print(f"OS Name: {temp['scan'][target_ip]['osmatch'][0]['name']}")
-#    # Print the scanned ports and their service/version information
-#    for port, info in temp['scan'][target_ip]['tcp'].items():
-#        print(f"Port {port}: {info['name']} - {info['product']} {info['version']}")
-#
-#if __name__ == "__main__":
-#    target_ip = input("Enter the target IP address: ")
-#
-#    scan_for_vulnerabilities(target_ip)
-
-
-
-# to find range of IPs
-#nmap -sN -PE 192.168.84.129-140
-
 import nmap
+
+
+def calculate_vulnerability_score(scan_result):
+    vulnerability_score = 0
+
+    if 'tcp' in scan_result:
+        vulnerability_score = len(scan_result['tcp'].items())
+
+    return vulnerability_score
+
+
+def find_most_vulnerable_server(scan_results):
+    most_vulnerable_server = None
+    highest_vulnerability_score = 0
+
+    for target_ip, scan_result in scan_results.items():
+        vulnerability_score = calculate_vulnerability_score(scan_result)
+
+        if vulnerability_score > highest_vulnerability_score:
+            highest_vulnerability_score = vulnerability_score
+            most_vulnerable_server = target_ip
+
+    return most_vulnerable_server, highest_vulnerability_score
+
 
 def scan_for_vulnerabilities(target_ip):
     nm = nmap.PortScanner()
@@ -43,19 +36,30 @@ def scan_for_vulnerabilities(target_ip):
         print(f"Host is up: {scan_result['status']['state']}")
 
         if 'osmatch' in scan_result:
-            print("Operating System Information:",scan_result['osmatch'][0]['name'])
+            print(
+                f"Operating System Information: {scan_result['osmatch'][0]['name']}")
 
         if 'tcp' in scan_result:
             print("Open Ports:")
             for port, port_info in scan_result['tcp'].items():
-                print(f"  - Port {port}: {port_info['name']} - {port_info['product']} - {port_info['version']}")
+                print(
+                    f"  - Port {port}: {port_info['name']} - {port_info['product']} - {port_info['version']}")
 
-        print("="*50)  # Separator between hosts
-        
+        print("=" * 50)  # Separator between hosts
+
+    # Find the most vulnerable server
+    most_vulnerable_server, highest_vulnerability_score = find_most_vulnerable_server(
+        temp['scan'])
+
+    print(f"The most vulnerable server is: {most_vulnerable_server}")
+    print(f"Highest Vulnerability Score: {highest_vulnerability_score}")
+
+
 if __name__ == "__main__":
     target_ip = input("Enter the target IP address: ")
 
     scan_for_vulnerabilities(target_ip)
 
 
-
+# to find range of IPs
+# nmap -sN -PE 192.168.84.129-140
